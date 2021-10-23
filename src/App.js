b193e25ac4ceb1383f63f5f3513abdfb434f7d47
EkramulHaqueEngineer <ekramulhaqueengineer@gmail.com>
2021-10-24 04:19:57 +0600

    added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+import { getAuth,signInWithPopup,GoogleAuthProvider,signOut } from "firebase/auth";
+import initializeAuthentication from './firebase/Firebase.init';
+import {useState} from 'react';
 
+initializeAuthentication();
+
+const provider = new GoogleAuthProvider();
 function App() {
+ const [user,setUser]  = useState({})
+  const handleGoogleSignIn = () => {
+    const auth = getAuth();
+    signInWithPopup(auth,provider)
+    .then(result => {
+      const {displayName,email,photoURL}= result.user;
+      const loggedInuser = {
+
+      name:displayName,
+      email:email,
+      photo:photoURL
+
+      }
+      setUser(loggedInuser)
+    
+  })
+}
+const handleSignOut = () =>{
+  const auth = getAuth();
+  signOut(auth).then(() => {
+  setUser({});
+  }).catch((error) => {
+   
+  });
+
+}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        !user.name ?
+        <div>
+      <button onClick={handleGoogleSignIn}>Google Sign In</button>
+      </div>:
+      
+      <button onClick={handleSignOut}>Sign Out</button>
+      
+}
+      <br></br>
+      {
+        user.name && <div>
+<h2>Welcome Dear {user.name}</h2>
+<img src={user.photo}></img>
+
+          </div>
+      }
     </div>
   );
 }
-
 export default App;
---
+++ src/firebase/Firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAj5Avdt3cUBZlhJYzk63N_7aA0_Nz3LMk",
+    authDomain: "simple-firebase-authenti-44426.firebaseapp.com",
+    projectId: "simple-firebase-authenti-44426",
+    storageBucket: "simple-firebase-authenti-44426.appspot.com",
+    messagingSenderId: "242032150618",
+    appId: "1:242032150618:web:3113fb445ec15c936b7063",
+    measurementId: "G-9MJNWT7F82"
+  };
+  export default firebaseConfig;
+  
